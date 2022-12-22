380873fab7ea51a4e3af2b5a170e43c993053909
Amit Kumar <amit.siwach@yahoo.com>
2022-12-22 08:56:54 +0530

    Added Cartpole Problem

--- HelloWorld/src/my_example.py
+++ HelloWorld/src/my_example.py
@@ -4,27 +4,30 @@ import time
 
 env = gym.make("CartPole-v1")
 
-
 def basic_policy(PoleAngle):
-    if PoleAngle < 0: # falling right
-        return 0 # move right
-    return 1 # move left
+    if PoleAngle <0: #Falling right
+        return 0  #move right
+    return 1  #move left
 
 total_rewards = list()
-N_episodes = 200
+N_episodes = 50
 N_steps = 200
+
 for episode in range(N_episodes):
     rewards = 0
-    CartPosition, CartVelocity, PoleAngle, PoleAngluarVelocity = env.reset()[0]
-    for step in range(N_steps):
+    CartPosition, CartVelocity, PoleAngle, PoleAngularVelocity = env.reset()[0]
+    for steps in range(N_steps):
         env.render()
         action = basic_policy(PoleAngle)
+        env.step(action)
         Observation, reward, done, info = env.step(action)[0:4]
         time.sleep(0.001)
         rewards += reward
-        if done:
+
+        if done: #means if it is collapsed.
             break
     total_rewards.append(rewards)
+
 
 stats = {
     "mean": np.mean(total_rewards),
@@ -34,3 +37,4 @@ stats = {
 }
 
 print(f"Final stats: \n{stats}")
+
